e9f0a5df5e96054f71e8cf7414157e0eb689e97c
rishal <devrishal@gmail.com>
2024-02-29 18:27:52 +0100

    upgrade springboot to 3.1.5 , jdk to 17 and open api ui to 2.3.0

--- src/main/java/com/rds/barcodegen/api/rest/DocumentResource.java
+++ src/main/java/com/rds/barcodegen/api/rest/DocumentResource.java
@@ -2,17 +2,18 @@ package com.rds.barcodegen.api.rest;
 
 import com.rds.barcodegen.api.swagger.DocumentResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface DocumentResource {
     @Operation(summary = "Download PDF with Code39 Barcode")
     @DocumentResponse
-    @GetMapping(path = "/download", produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(path = "/download", produces = { MediaType.APPLICATION_OCTET_STREAM_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public void downloadFile(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException;
 }
